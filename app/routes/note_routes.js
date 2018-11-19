9baadd4c6dc436a0c2dd4f0b9063d827a2ccf3e1
Chris <chris.northwood@smashpotatoes.com>
2018-11-19 10:27:17 -0800

    updated routes to be less 'note' focused

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,7 +2,7 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
-  app.get('/notes/:id', (req, res) => {
+  app.get('/item/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     db.collection('notes').findOne(details, (err, item) => {
@@ -14,19 +14,19 @@ module.exports = function(app, db) {
     });
   });
 
-  app.delete('/notes/:id', (req, res) => {
+  app.delete('/item/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     db.collection('notes').remove(details, (err, item) => {
       if (err) {
         res.send({ 'error': 'An error has occured' })
       } else {
-        res.send('Note ' + id + ' deleted!');
+        res.send('Item ' + id + ' deleted!');
       }
     });
   });
 
-  app.put('/notes/:id', (req, res) => {
+  app.put('/item/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     const note = { text: req.body.body, title: req.body.title }
@@ -39,7 +39,7 @@ module.exports = function(app, db) {
     });
   });
 
-  app.post('/notes', (req, res) => {
+  app.post('/item', (req, res) => {
     const note = { text: req.body.body, title: req.body.title }
     db.collection('notes').insert(note, (err, results) => {
       if (err) {
